ac71cdd12622a9f6150fb462a2ec55d1dc45302e
Rodrigo Adler <rodrigoadler8@gmail.com>
2024-11-13 23:16:31 -0300

    Adiciona arquivo questao1.c

---
+++ questao1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+
+unsigned char day;
+
+    while (day = 1) {
+        printf("Enter a day of the week:\n ");
+        scanf("%hhu", &day);
+
+        if (day == 0) break;
+
+        switch (day) {
+            case 1:
+            printf("Monday!\n");
+            break;
+            case 2:
+            printf("Tuesday!\n");
+            break;
+            case 3:
+            printf("Wednesday!\n");
+            break;
+            case 4:
+            printf("Thursday!\n");
+            break;
+            case 5:
+            printf("Friday!\n");
+            break;
+            case 6:
+            printf("Saturday!\n");
+            break;
+            case 7: 
+            printf("Sunday!\n");
+            break;
+            default:
+            printf("Invalid day number!\n");
+
+    } 
+
+}
+    printf("End...");
+    
+    return 0;
+
+}
